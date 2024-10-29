cc16eb320daee84cc8104ed71586cfc4f50284af
Margarita-Simeonova <margito.sl@gmail.com>
2024-10-29 22:59:50 +0200

    Tennis Ranklist
    Tennis Ranklist

---
+++ 08. Tennis Ranklist.py
@@ -0,0 +1,30 @@
+W = 2000
+F = 1200
+SF = 720
+
+
+def tennis_ranking_list(tournament_count, points):
+    points_earned = 0
+    race_earned = 0
+
+    for _ in range(tournament_count):
+        ranking = input()
+
+        if ranking == "W":
+            points_earned += 2000
+            race_earned += 1
+        elif ranking == "F":
+            points_earned += 1200
+        elif ranking == "SF":
+            points_earned += 720
+
+    final_points = points_earned + points
+    average_points = int(points_earned / tournament_count)
+    percent = race_earned / tournament_count * 100
+
+    return f"Final points: {final_points}\nAverage points: {average_points}\n{percent:.2f}%"
+
+
+count = int(input())
+points = int(input())
+print(tennis_ranking_list(count, points))
